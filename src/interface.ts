d1dcb467b27cf1d32af794759dd7b2eb56081c20
Mayank <mk20040307@gmail.com>
2026-01-15 23:41:33 +0530

    learn objects and interface deeply

--- src/interface.ts
+++ src/interface.ts
@@ -1,3 +1,21 @@
+// why type?
+function makeChai(order: {type: string, cups: number, sugar: boolean}){
+    console.log("making chai...")
+}
+function chaiOrder(order: {type: string, cups: number, sugar: boolean}){
+    console.log("Chai order received")
+}
+// here we again and agin writing same type
+type Chai1 = {
+    type: string,
+    cups: number,
+    sugar: boolean
+}
+function newChaiOrder(order: Chai1){}
+
+
+
+
 type chai={
     water: number
     milk: number
@@ -8,10 +26,11 @@ class masalaChai implements chai{  // show no error
     milk = 100
 }
 
+//this type can't be used for implementing in class reason is below
+
 type cupSize = "small" | "medium" | "large"
 // class chai implements cupSize{  // error because cupSize is not chai type
-//     water = 100
-//     milk = 100
+//     cupSize: "small"
 // }
 
 // so use interface for such cases
@@ -27,4 +46,11 @@ class Chai implements cup_size{
     size: "small" | "medium" | "large" = "medium"
 }
 
-// in class implement use interface not type
+// in class implement use interface not type(it is a general statement)
+
+
+type TeaType = "masala" | "ginger" | "lemon" // union type
+function tea2(t: TeaType){  // 
+    console.log(t)  // only masala, ginger, lemon can be passed
+}
+
--- src/object.ts
+++ src/object.ts
@@ -1,3 +1,14 @@
+let tea : {
+    name: string,
+    price: number,
+    isHot: boolean
+}
+
+tea = {
+    name: "ginger",
+    price: 30,
+    isHot: true
+}
 
 
 type Tea = {
@@ -12,17 +23,51 @@ const adrakChai: Tea = {
     ingredients: ["water", "milk", "tea leaves", "ginger", "sugar"]
 }
 
-type chai = {size: string}
 
+
+type chai = {size: string}
 //const normalChai: chai = {size: "200ml", isHot: true} // through error bcoz type of normalChai is chai but
 const normalChai = {size: "300ml", isHot: true}
 const newChai: chai = normalChai
 
 type Item = {name: string, quantity: number}
-type address = {street: string, zip: number}
+type address = {street: string, pin: number}
 
 type order = {
     id: string,
-    item: Item,
+    item: Item[], // array of Item type
     shippingAddress: address
 }
+
+type chai2 = {
+    name: string,
+    price: number,
+    isHot: boolean
+}
+
+const updateChai = (updates: Partial<chai2>) => {
+    console.log("updating...")
+}
+updateChai({price: 40})  // we can pass either name or price or both
+
+const readChai = (chai: Readonly<chai2>) => {
+    // chai.price=50  // THROUGH error bcoz chai is readonly
+    console.log(`Chai name is ${chai.name} and price is ${chai.price}`)
+}
+
+readChai({name: "masala", price: 30, isHot: true})
+
+const func = (obj: Required<chai2>) => {
+    console.log("nabsdhjksm...")
+}
+//func({name: "mayank"})  // THROUGH error bcoz all properties are required in Required<> so price is also necessary
+func({name: "mayank", price: 20, isHot: false})
+
+type chaiPicked = Pick<chai2, "name" | "isHot">  // only name and isHot property is picked
+const pickedChai: chaiPicked = {
+    name: "ginger",
+    isHot: true,
+    // price: 30  // THROUGH error bcoz price is not picked
+}
+
+// similarly we have Omit<> that omits the specified properties i.e excludes them and cannot be used
